f5e9acd27a050ad6cc415208cb2eb752d1c7f089
Destin Estrela <destin.estrela@gmail.com>
2022-04-25 09:12:12 -0700

    support for adding multiple objects

--- main.py
+++ main.py
@@ -5,45 +5,62 @@ from PySide2.Qt3DExtras import Qt3DExtras
 from PySide2.Qt3DRender import Qt3DRender
 from PySide2.Qt3DInput import Qt3DInput
 
-# SOURCES
+# SOURCES: Anything besides QT documentation listed here 
 # https://stackoverflow.com/questions/60585973/pyside2-qt3d-mesh-does-not-show-up
 # https://wiki.qt.io/Qt_for_Python_Tutorial_ClickableButton
+# https://stackoverflow.com/questions/38923978/object-going-out-of-scope-and-being-garbage-collected-in-pyside-pyqt
 
-class SceneModifier(QtCore.QObject):
+class Sphere(QtCore.QObject):
     def __init__(self, root_entity=None, cameraEntity=None):
         super().__init__()
         self.m_rootEntity = root_entity
         self.m_cameraEntity = cameraEntity
+        self.m_defaultMaterial = Qt3DExtras.QPhongMaterial(
+            diffuse=QtGui.QColor("#665423"))
+       
+        self.sphereMesh = Qt3DExtras.QSphereMesh(
+            rings=20, slices=20, radius=2)
 
-    def createCube(self):
+        self.sphereTransform = Qt3DCore.QTransform(
+            scale=1.3, translation=self.m_cameraEntity.viewCenter(),
+        )
+
+        self.m_sphereEntity = Qt3DCore.QEntity(self.m_rootEntity)
+        self.m_sphereEntity.addComponent(self.sphereMesh)
+        self.m_sphereEntity.addComponent(self.m_defaultMaterial)
+        self.m_sphereEntity.addComponent(self.sphereTransform)
+        
+
+class Cube(QtCore.QObject):
+    def __init__(self, root_entity=None, cameraEntity=None):
+        super().__init__()
+        self.m_rootEntity = root_entity
+        self.m_cameraEntity = cameraEntity
         self.cuboid = Qt3DExtras.QCuboidMesh()
-
         self.cuboidTransform = Qt3DCore.QTransform(
             scale=4.0, translation=self.m_cameraEntity.viewCenter(),
         )
-
-        self.cuboidMaterial = Qt3DExtras.QPhongMaterial(diffuse=QtGui.QColor("#665423"))
+        self.cuboidMaterial = Qt3DExtras.QPhongMaterial(
+            diffuse=QtGui.QColor("#665423"))
 
         self.m_cuboidEntity = Qt3DCore.QEntity(self.m_rootEntity)
         self.m_cuboidEntity.addComponent(self.cuboid)
         self.m_cuboidEntity.addComponent(self.cuboidMaterial)
         self.m_cuboidEntity.addComponent(self.cuboidTransform)
 
-    
+class SceneModifier(QtCore.QObject):
+    def __init__(self, root_entity=None, cameraEntity=None):
+        super().__init__()
+        self.m_rootEntity = root_entity
+        self.m_cameraEntity = cameraEntity
+        self.m_objectList = []
+
+    def createCube(self):
+        self.m_objectList.append(
+            Cube(self.m_rootEntity, self.m_cameraEntity))
+
     def createSphere(self):
-        self.sphereMesh = Qt3DExtras.QSphereMesh(
-             rings=20, slices=20, radius=2)
-
-        self.sphereTransform = Qt3DCore.QTransform(
-            scale=1.3, translation=self.m_cameraEntity.viewCenter(),
-        )
-
-        self.sphereMaterial = Qt3DExtras.QPhongMaterial(diffuse=QtGui.QColor("#a69929"))
-
-        self.m_sphereEntity = Qt3DCore.QEntity(self.m_rootEntity)
-        self.m_sphereEntity.addComponent(self.sphereMesh)
-        self.m_sphereEntity.addComponent(self.sphereMaterial)
-        self.m_sphereEntity.addComponent(self.sphereTransform)
+        self.m_objectList.append(Sphere(self.m_rootEntity, self.m_cameraEntity))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -88,7 +105,6 @@ if __name__ == "__main__":
     camController = Qt3DExtras.QOrbitCameraController(rootEntity)
     camController.setCamera(cameraEntity)
     
-
     modifier = SceneModifier(rootEntity, cameraEntity)
     print(cameraEntity.position)
 
