a1d778703cb9e24e0629c1ac1ec1a06102cef0ac
Destin Estrela <destin.estrela@gmail.com>
2022-04-26 07:31:43 -0700

    reorganized main

--- SceneEditor.py
+++ SceneEditor.py
@@ -7,7 +7,9 @@ from PySide2.Qt3DInput import Qt3DInput
 from Primitives import * 
 from PrimitiveEditorWidgets import * 
 from PrimitiveListItems import * 
+
 # SOURCES: Anything besides QT documentation listed here 
+# https://www.tutorialspoint.com/pyqt/pyqt_qstackedwidget.htm
 # https://stackoverflow.com/questions/60585973/pyside2-qt3d-mesh-does-not-show-up
 # https://wiki.qt.io/Qt_for_Python_Tutorial_ClickableButton
 # https://stackoverflow.com/questions/38923978/object-going-out-of-scope-and-being-garbage-collected-in-pyside-pyqt
@@ -29,6 +31,7 @@ class ShapeEditor(QtCore.QObject):
         self.m_objectListWidget.itemClicked.connect(self.initPrimitiveEditorWidget)
 
     def createCube(self):
+        print("created cube")
         cube = Cube(self.m_rootEntity, self.m_cameraEntity)
         cubeListItem = CubeListItem(cube.m_displayName, cube)
         self.initPrimitiveEditorWidget(cubeListItem)
@@ -75,39 +78,18 @@ class ShapeEditor(QtCore.QObject):
             self.m_objectListWidget.addItem(listItem)
 
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-
-    view = Qt3DExtras.Qt3DWindow()
-    view.defaultFrameGraph().setClearColor(QtGui.QColor("#4d4d4f"))
-    container = QtWidgets.QWidget.createWindowContainer(view)
-    screenSize = view.screen().size()
-    container.setMinimumSize(QtCore.QSize(200, 100))
-    container.setMaximumSize(screenSize)
-
-    widget = QtWidgets.QWidget()
-    hLayout = QtWidgets.QHBoxLayout(widget)
-    
-    ##
-    rightWindow = QtWidgets.QVBoxLayout()
-    rightWindow.setAlignment(QtCore.Qt.AlignTop)
-
-    hLayout.addLayout(rightWindow)
-    hLayout.addWidget(container, 1)
-
-    widget.setWindowTitle("3D Editor")
-
-    input_ = Qt3DInput.QInputAspect()
-    view.registerAspect(input_)
-
-    rootEntity = Qt3DCore.QEntity()
-
+def initialize_camera(view, rootEntity):
     cameraEntity = view.camera()
     cameraEntity.lens().setPerspectiveProjection(45.0, 16.0 / 9.0, 0.1, 1000.0)
     cameraEntity.setPosition(QtGui.QVector3D(0, 0, 20.0))
     cameraEntity.setUpVector(QtGui.QVector3D(0, 1, 0))
     cameraEntity.setViewCenter(QtGui.QVector3D(0, 0, 0))
+    camController = Qt3DExtras.QOrbitCameraController(rootEntity)
+    camController.setCamera(cameraEntity)
+    return cameraEntity
 
+
+def initialize_lighting(rootEntity, cameraEntity):
     lightEntity = Qt3DCore.QEntity(rootEntity)
     light = Qt3DRender.QPointLight(lightEntity)
     light.setColor("white")
@@ -118,30 +100,75 @@ if __name__ == "__main__":
     lightTransform.setTranslation(cameraEntity.position())
     lightEntity.addComponent(lightTransform)
 
-    camController = Qt3DExtras.QOrbitCameraController(rootEntity)
-    camController.setCamera(cameraEntity)
-    objectList = QtWidgets.QListWidget(widget)
 
-    shapeEditor = ShapeEditor(rootEntity, cameraEntity, objectList, hLayout)
+class PrimitiveCreatorWidget(QtWidgets.QWidget):
+    def __init__(self, rootEntity, cameraEntity):
+        QtWidgets.QWidget.__init__(self)
+        layout = QtWidgets.QVBoxLayout()
+        layout.setAlignment(QtCore.Qt.AlignTop)
+        self.setLayout(layout)
+        self.setMinimumSize(100, 100)
+        self.setMaximumWidth(200)
 
+        self.objectList = QtWidgets.QListWidget(self)
+        self.shapeEditor = ShapeEditor(rootEntity, cameraEntity, self.objectList, layout)
+
+        createCubeButton = QtWidgets.QPushButton(self)
+        createCubeButton.setText("Create Cube")
+
+        createSphereButton = QtWidgets.QPushButton(self)
+        createSphereButton.setText("Create Sphere")
+
+        createCubeButton.clicked.connect(self.shapeEditor.createCube)
+        createSphereButton.clicked.connect(self.shapeEditor.createSphere)
+
+
+        layout.addWidget(createCubeButton)
+        layout.addWidget(createSphereButton)
+        layout.addWidget(self.objectList)
+
+       
+        self.shapeEditor.restoreData()
+        self.show()
+
+
+class Application(QtWidgets.QWidget):
+    def __init__(self, rootEntity, cameraEntity, container):
+        QtWidgets.QWidget.__init__(self)
+        layout = QtWidgets.QHBoxLayout()
+        self.setLayout(layout)
+
+    
+        self.shapeCreatorWidget = PrimitiveCreatorWidget(rootEntity, cameraEntity)
+        layout.addWidget(self.shapeCreatorWidget, 1)
+        layout.addWidget(container, 1)
+
+        self.setWindowTitle("3D Editor")
+        self.resize(1200, 800)
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    # init 3D environment
+    view = Qt3DExtras.Qt3DWindow()
+    view.defaultFrameGraph().setClearColor(QtGui.QColor("#4d4d4f"))
+    container = QtWidgets.QWidget.createWindowContainer(view)
+    screenSize = view.screen().size()
+    container.setMinimumSize(QtCore.QSize(400, 400))
+    container.setMaximumSize(screenSize)
+
+    rootEntity = Qt3DCore.QEntity()
+    cameraEntity = initialize_camera(view, rootEntity)
+    initialize_lighting(rootEntity, cameraEntity)
     view.setRootEntity(rootEntity)
 
-    createCubeButton = QtWidgets.QPushButton(widget)
-    createCubeButton.setText("Create Cube")
+    # init input
+    input_ = Qt3DInput.QInputAspect()
+    view.registerAspect(input_)
 
-    createSphereButton = QtWidgets.QPushButton(widget)
-    createSphereButton.setText("Create Sphere")
-
-    rightWindow.addWidget(createCubeButton)
-    rightWindow.addWidget(createSphereButton)
-    rightWindow.addWidget(objectList)
-
-    createCubeButton.clicked.connect(shapeEditor.createCube)
-    createSphereButton.clicked.connect(shapeEditor.createSphere)
-
-    shapeEditor.restoreData()
-
-    widget.show()
-    widget.resize(1200, 800)
+    #init app
+    appWidget = Application(rootEntity, cameraEntity, container)
 
     sys.exit(app.exec_())
